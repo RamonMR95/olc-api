77a2028a332350fff435cce51b783b6f45559d7b
Ramon Moñino <ramonmr16@gmail.com>
2020-04-07 21:02:51 +0200

    Initial commit

---
+++ src/test/java/com/ramonmr95/tiky/olc/OlcApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramonmr95.tiky.olc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlcApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
