2c1c97f8322d650320f419ee0c802d51bf7eb5d9
Ramon Moñino <ramonmr16@gmail.com>
2020-04-08 13:16:00 +0200

    Added User entity.

---
+++ src/main/java/com/ramonmr95/tiky/olc/entities/User.java
@@ -0,0 +1,129 @@
+package com.ramonmr95.tiky.olc.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "users")
+public class User implements Serializable {
+
+	private static final long serialVersionUID = 2781452882301619960L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+	@JoinColumn(name = "address_id", nullable = false)
+	private Address address;
+
+	@OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+	@JoinColumn(name = "role_id", nullable = false)
+	private Role role;
+
+	@OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+	@JoinColumn(name = "course_id", nullable = true)
+	private Course course;
+
+	@NotNull(message = "The name is required")
+	@Column(nullable = false)
+	private String name;
+
+	@NotNull(message = "The surname is required")
+	@Column(name = "sur_name", nullable = false)
+	private String surName;
+
+	@NotNull(message = "The nickname is required")
+	@Column(name = "nick_name", nullable = false, unique = true)
+	private String nickName;
+
+	@NotNull(message = "The email is required")
+	@Email(message = "Invalid email format")
+	@Column(nullable = false, unique = true)
+	private String email;
+
+	@NotNull(message = "The password is required")
+	@Column(nullable = false)
+	private String password;
+
+	@NotNull(message = "The active state is required")
+	@Column(nullable = false)
+	private boolean active;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	public Role getRole() {
+		return role;
+	}
+
+	public void setRole(Role role) {
+		this.role = role;
+	}
+
+	public Course getCourse() {
+		return course;
+	}
+
+	public void setCourse(Course course) {
+		this.course = course;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public boolean isActive() {
+		return active;
+	}
+
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+
+}
