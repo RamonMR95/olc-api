f5f32ead8fd1739c199ff79a6685fff1dd17cc39
Antonio Ruiz Marin <Antonio507murcia@gmail.com>
2020-05-19 18:20:01 +0200

    Add Interface with CrudRepository for IExamDao and ISubjectDao

---
+++ src/main/java/com/ramonmr95/tiky/olc/repositories/ISubjectDao.java
@@ -0,0 +1,9 @@
+package com.ramonmr95.tiky.olc.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.ramonmr95.tiky.olc.entities.Subject;
+
+public interface ISubjectDao extends CrudRepository<Subject, Long> {
+
+}
