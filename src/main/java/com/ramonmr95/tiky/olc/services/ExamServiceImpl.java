d316bc506a58b61ecfb747a5f9eaeaff06eb02f5
Antonio Ruiz Marin <Antonio507murcia@gmail.com>
2020-05-21 17:33:29 +0200

    Added ValidationsException and DataNotFoundException and Queries.

--- src/main/java/com/ramonmr95/tiky/olc/services/ExamServiceImpl.java
+++ src/main/java/com/ramonmr95/tiky/olc/services/ExamServiceImpl.java
@@ -7,7 +7,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.ramonmr95.tiky.olc.entities.Exam;
+import com.ramonmr95.tiky.olc.exceptions.DataNotFoundException;
+import com.ramonmr95.tiky.olc.exceptions.EntityValidationException;
 import com.ramonmr95.tiky.olc.repositories.IExamDao;
+import com.ramonmr95.tiky.olc.validators.EntityValidator;
 
 @Service
 public class ExamServiceImpl implements IExamService {
@@ -15,28 +18,43 @@ public class ExamServiceImpl implements IExamService {
 	@Autowired
 	private IExamDao examDao;
 
-	@Transactional
+	private EntityValidator<Exam> entityValidator = new EntityValidator<>();
+
+	@Transactional(readOnly = true)
 	@Override
 	public List<Exam> findAll() {
-		return (List<Exam>) this.examDao.findAll();
+		return (List<Exam>) examDao.findAll();
+	}
+
+	@Transactional(readOnly = true)
+	@Override
+	public Exam findOne(Long id) throws DataNotFoundException {
+		if (id != null) {
+			return this.examDao.findById(id)
+					.orElseThrow(() -> new DataNotFoundException("Exam with id: " + id + " could not be found."));
+		}
+		throw new DataNotFoundException("Error Exam id is required");
 	}
 
 	@Transactional
 	@Override
-	public Exam findOne(Long id) {
-		return this.examDao.findById(id).orElse(null);
+	public Exam save(Exam exam) throws EntityValidationException {
+		if (this.entityValidator.isEntityValid(exam)) {
+			return this.examDao.save(exam);
+		}
+		throw new EntityValidationException(this.entityValidator.getEntityValidationErrorsString(exam));
 	}
 
-	@Transactional(readOnly = true)
+	@Transactional
 	@Override
-	public Exam save(Exam exam) {
-		return this.examDao.save(exam);
-	}
-
-	@Transactional(readOnly = true)
-	@Override
-	public void delete(Long id) {
+	public void delete(Long id) throws DataNotFoundException {
+		this.findOne(id);
 		this.examDao.deleteById(id);
+	}
+
+	@Override
+	public List<Exam> findAllExamByCourseIdAndSubjectId(Long course_id, Long subject_id) {
+		return this.examDao.findAllExamByCourseIdAndSubjectId(course_id, subject_id);
 	}
 
 }
--- src/main/java/com/ramonmr95/tiky/olc/services/SubjectServiceImpl.java
+++ src/main/java/com/ramonmr95/tiky/olc/services/SubjectServiceImpl.java
@@ -7,7 +7,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.ramonmr95.tiky.olc.entities.Subject;
+import com.ramonmr95.tiky.olc.exceptions.DataNotFoundException;
+import com.ramonmr95.tiky.olc.exceptions.EntityValidationException;
 import com.ramonmr95.tiky.olc.repositories.ISubjectDao;
+import com.ramonmr95.tiky.olc.validators.EntityValidator;
 
 @Service
 public class SubjectServiceImpl implements ISubjectService {
@@ -15,28 +18,46 @@ public class SubjectServiceImpl implements ISubjectService {
 	@Autowired
 	private ISubjectDao subjectDao;
 
-	@Transactional
+	private EntityValidator<Subject> entityValidator = new EntityValidator<>();
+
+	@Transactional(readOnly = true)
 	@Override
 	public List<Subject> findAll() {
 		return (List<Subject>) this.subjectDao.findAll();
 	}
 
+	@Transactional(readOnly = true)
+	@Override
+	public Subject findOne(Long id) throws DataNotFoundException {
+
+		if (id != null) {
+			return this.subjectDao.findById(id)
+					.orElseThrow(() -> new DataNotFoundException("Subject with id: " + id + " could not be found."));
+		}
+
+		throw new DataNotFoundException("Error Subject id is required");
+	}
+
 	@Transactional
 	@Override
-	public Subject findOne(Long id) {
-		return this.subjectDao.findById(id).orElse(null);
+	public Subject save(Subject subject) throws EntityValidationException {
+		if (this.entityValidator.isEntityValid(subject)) {
+			return this.subjectDao.save(subject);
+		}
+		throw new EntityValidationException(this.entityValidator.getEntityValidationErrorsString(subject));
 	}
 
-	@Transactional(readOnly = true)
+	@Transactional
 	@Override
-	public Subject save(Subject subject) {
-		return this.subjectDao.save(subject);
-	}
-
-	@Transactional(readOnly = true)
-	@Override
-	public void delete(Long id) {
+	public void delete(Long id) throws DataNotFoundException {
+		this.findOne(id);
 		this.subjectDao.deleteById(id);
+	}
+
+	@Transactional(readOnly = true)
+	@Override
+	public List<Subject> findSubjectByCourseId(Long id) {
+		return this.subjectDao.findSubjectByCourseId(id);
 	}
 
 }
