8cd353471e74f337b5bd5e89646db5671141f7b3
Ramon Moñino <ramonmr16@gmail.com>
2020-05-21 16:32:31 +0200

    Added Custom exceptions.

---
+++ src/main/java/com/ramonmr95/tiky/olc/exceptions/DataNotFoundException.java
@@ -0,0 +1,15 @@
+package com.ramonmr95.tiky.olc.exceptions;
+
+public class DataNotFoundException extends Exception {
+
+	private static final long serialVersionUID = -3255740338118296997L;
+
+	public DataNotFoundException() {
+
+	}
+
+	public DataNotFoundException(String message) {
+		super(message);
+	}
+
+}
