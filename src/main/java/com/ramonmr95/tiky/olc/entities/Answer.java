5fc2735e461ddbe3e8be80d8ae5839bb03e36563
Ramon Moñino <ramonmr16@gmail.com>
2020-05-25 19:31:52 +0200

    Added Answer, Question, Topic, TopicContent entities.

---
+++ src/main/java/com/ramonmr95/tiky/olc/entities/Answer.java
@@ -0,0 +1,61 @@
+package com.ramonmr95.tiky.olc.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "answers")
+public class Answer implements Serializable {
+
+	private static final long serialVersionUID = -2400376217689509568L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String answer;
+
+	private boolean correct;
+
+	@ManyToOne
+	private Question question;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getAnswer() {
+		return answer;
+	}
+
+	public void setAnswer(String answer) {
+		this.answer = answer;
+	}
+
+	public boolean isCorrect() {
+		return correct;
+	}
+
+	public void setCorrect(boolean correct) {
+		this.correct = correct;
+	}
+
+	public Question getQuestion() {
+		return question;
+	}
+
+	public void setQuestion(Question question) {
+		this.question = question;
+	}
+
+}
---
+++ src/main/java/com/ramonmr95/tiky/olc/entities/Topic.java
@@ -0,0 +1,61 @@
+package com.ramonmr95.tiky.olc.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "topics")
+public class Topic implements Serializable {
+
+	private static final long serialVersionUID = 942906046676052763L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String name;
+
+	private String description;
+
+	@ManyToOne
+	private Subject subject;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Subject getSubject() {
+		return subject;
+	}
+
+	public void setSubject(Subject subject) {
+		this.subject = subject;
+	}
+
+}
