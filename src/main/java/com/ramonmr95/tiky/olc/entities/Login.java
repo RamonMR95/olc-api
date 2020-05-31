b464bc6c0dff8c88535865d7cd98e82d1e14ba36
Ramon Moñino <ramonmr16@gmail.com>
2020-05-31 18:57:24 +0200

    Changed login email to username.

--- src/main/java/com/ramonmr95/tiky/olc/entities/Login.java
+++ src/main/java/com/ramonmr95/tiky/olc/entities/Login.java
@@ -2,24 +2,24 @@ package com.ramonmr95.tiky.olc.entities;
 
 public class Login {
 
-	private String email;
+	private String username;
 
 	private String password;
 
 	public Login() {
 	}
 
-	public Login(String email, String password) {
-		this.email = email;
+	public Login(String username, String password) {
+		this.username = username;
 		this.password = password;
 	}
 
-	public String getEmail() {
-		return email;
+	public String getUsername() {
+		return username;
 	}
 
-	public void setEmail(String email) {
-		this.email = email;
+	public void setUsername(String username) {
+		this.username = username;
 	}
 
 	public String getPassword() {
