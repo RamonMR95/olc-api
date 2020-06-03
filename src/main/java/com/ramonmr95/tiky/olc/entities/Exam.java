6b785188a593b0afbff795a4a2d95f779259072d
Ramon Moñino <ramonmr16@gmail.com>
2020-06-03 16:38:54 +0200

    Fixed entities.

--- src/main/java/com/ramonmr95/tiky/olc/entities/Exam.java
+++ src/main/java/com/ramonmr95/tiky/olc/entities/Exam.java
@@ -16,22 +16,23 @@ import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 
-import org.modelmapper.ModelMapper;
-
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.ramonmr95.tiky.olc.dtos.ExamDto;
 
 @Entity
 @Table(name = "exams")
 public class Exam implements Serializable {
 
-	private static final long serialVersionUID = 3706876187186768150L;
+	private static final long serialVersionUID = 3706876187182768150L;
 	private static final String dateFormat = "yyyy-MM-dd";
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@NotNull(message = "The name is required")
+	@Column(nullable = false)
+	private String name;
 
 	@JsonIgnore
 	@OneToOne
@@ -48,18 +49,20 @@ public class Exam implements Serializable {
 	@JsonFormat(pattern = dateFormat)
 	private Date date;
 
-	private Double mark;
-
-	@ManyToOne(optional = true)
-	@JoinColumn(name = "user_id", nullable = true)
-	private User user;
-
 	public Long getId() {
 		return id;
 	}
 
 	public void setId(Long id) {
 		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
 	}
 
 	public Subject getSubject() {
@@ -85,25 +88,5 @@ public class Exam implements Serializable {
 	public void setDate(Date date) {
 		this.date = date;
 	}
-
-	public Double getMark() {
-		return mark;
-	}
-
-	public void setMark(Double mark) {
-		this.mark = mark;
-	}
-
-	public User getUser() {
-		return user;
-	}
-
-	public void setUser(User user) {
-		this.user = user;
-	}
-
-	public ExamDto convertToDto() {
-		return new ModelMapper().map(this, ExamDto.class);
-	}
-
+	
 }
