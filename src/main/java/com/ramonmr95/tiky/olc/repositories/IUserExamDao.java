1f4bbded72f1104345c03f2fd9d10253e3e6b135
Ramon Moñino <ramonmr16@gmail.com>
2020-06-04 15:09:29 +0200

    Added query to find out if the user had already done the exam.

--- src/main/java/com/ramonmr95/tiky/olc/repositories/IUserExamDao.java
+++ src/main/java/com/ramonmr95/tiky/olc/repositories/IUserExamDao.java
@@ -1,9 +1,12 @@
 package com.ramonmr95.tiky.olc.repositories;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.ramonmr95.tiky.olc.entities.UserExam;
 
 public interface IUserExamDao extends CrudRepository<UserExam, Long>{
 
+	@Query("SELECT e FROM UserExam e WHERE e.user.id = ?1 AND e.exam.id = ?2")
+	public UserExam findExamByUserId(Long id, Long examId);
 }
