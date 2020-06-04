190404581c64507edc012e90ca08a06641883103
Ramon Moñino <ramonmr16@gmail.com>
2020-06-04 19:42:50 +0200

    Added query to the get exams of an user.

--- src/main/java/com/ramonmr95/tiky/olc/controllers/UserExamController.java
+++ src/main/java/com/ramonmr95/tiky/olc/controllers/UserExamController.java
@@ -3,9 +3,11 @@ package com.ramonmr95.tiky.olc.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ramonmr95.tiky.olc.entities.UserExam;
@@ -35,6 +37,20 @@ public class UserExamController {
 			return new ResponseEntity<>(this.parser.parseJsonToMap(e.getMessage()), HttpStatus.NOT_FOUND);
 		}
 
+	}
+
+	@GetMapping("/exam")
+	public ResponseEntity<?> findExamByUserId(@RequestParam(name = "user_id") Long userId,
+			@RequestParam(name = "exam_id") Long examId) {
+		try {
+			UserExam userExam = this.userExamService.findExamByUserId(userId, examId);
+			if (userExam != null) {
+				return new ResponseEntity<>(userExam, HttpStatus.OK);
+			}
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		} catch (DataNotFoundException e) {
+			return new ResponseEntity<>(this.parser.parseJsonToMap(e.getMessage()), HttpStatus.BAD_REQUEST);
+		}
 	}
 
 }
