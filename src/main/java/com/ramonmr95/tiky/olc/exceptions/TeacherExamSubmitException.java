e54d4c8e237f541cc7d5e6d836fde544eb9f6be1
Ramon Moñino <ramonmr16@gmail.com>
2020-06-06 14:16:24 +0200

    Added new exception thrown if a teacher attempts to submit an exam.

---
+++ src/main/java/com/ramonmr95/tiky/olc/exceptions/TeacherExamSubmitException.java
@@ -0,0 +1,15 @@
+package com.ramonmr95.tiky.olc.exceptions;
+
+public class TeacherExamSubmitException extends Exception {
+
+	private static final long serialVersionUID = 6797179662951582468L;
+
+	public TeacherExamSubmitException() {
+
+	}
+
+	public TeacherExamSubmitException(String msg) {
+		super(msg);
+	}
+
+}
